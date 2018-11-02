caa73d010f93de89d65c2740e0963a75c3e2f9c5
pinma1833427 <pinma1833427@etu.cegepjonuiere.ca>
2018-11-01 21:59:38 -0400

    exercise 9

--- JavaScript/exercise 9/JS/traitement.js
+++ JavaScript/exercise 9/JS/traitement.js
@@ -1,5 +1,15 @@
 function btnCalculer_onclick()
 { 
-
-
+ 	var NbEtu, NbNorm, NbSenior, NbBillets, Total;  //déclaration des variables
+	
+	NbEtu = parseInt(document.getElementById("txtBilletEtu").value);
+    NbSenior = parseInt(document.getElementById("txtBilletSenior").value);
+    NbNorm = parseInt(document.getElementById("txtBilletNorm").value);
+    NbBillets = NbNorm +  NbEtu + NbSenior;
+    Total = (NbNorm * 20) + (NbEtu * 12) + (NbSenior * 15);
+	if(NbBillets >= 5)
+	{
+		Total = Total * 0.90;
+	}
+ 	console.log("Votre nombre de billet est de: " + NbBillets + ", et votre total est de: " + Total + "$");   //affichage dans l'outil de débuggage du navigateur.
 }
