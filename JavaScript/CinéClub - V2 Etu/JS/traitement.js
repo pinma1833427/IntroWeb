49626a8705ab178c3ede30e8c4761d485104cf3e
pinma1833427 <pinma1833427@etu.cegepjonuiere.ca>
2018-11-14 10:51:09 -0500

    CinéClubApresDecoupageEtImage

--- JavaScript/CinéClub - V2 Etu/JS/traitement.js
+++ JavaScript/CinéClub - V2 Etu/JS/traitement.js
@@ -1,10 +1,15 @@
 function btnCalculer_onclick()
 {
-    var Age, Prix;
+    var Age, Prix, MiSession, Midi;
 
     Prix = parseInt(document.getElementById("txtBasePrix").value);
     Age = parseInt(document.getElementById("txtAge").value);
+    Calcule();
+    document.getElementById("lblMessage").innerHTML = "Le prix d'un billet du midi pour un spectateur de 16 ans est de: " + Prix + "$";
+}
 
+function Calcule()
+{
     if ((document.getElementById("chkMiSession").checked == true) && (document.getElementById("lstOptions").value == "Halloween"))
     {
         if(Age <= 16)
@@ -20,10 +25,17 @@ function btnCalculer_onclick()
         }
         else
         {
-
             Prix = Prix * 0.7;
-
         }
     }
-    document.getElementById("lblMessage").innerHTML = "Le prix d'un billet du midi pour un spectateur de 16 ans est de: " + Prix + "$";
+    return Prix;
+}
+
+function lstOptions_onchange()
+{
+    var film;
+
+    film = document.getElementById("lstOptions").value;
+    document.getElementById("imgAct").src="img/" + film + ".jpg";
+
 }
